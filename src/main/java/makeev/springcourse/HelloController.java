e2d9567f27628b07e344b16bc4d54f02fa222b08
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-03-13 17:37:23 +0300

    Hello World! with xml

---
+++ src/main/java/makeev/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package makeev.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!_index</h2>
+</body>
+</html>
